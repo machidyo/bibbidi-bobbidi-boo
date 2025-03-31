9e6c0f06627304dbe157216b096dbf3f5e835bd6
machidyo <yorihiko.machida@curiosity-inc.jp>
2025-03-31 17:56:06 +0900

    refactor: 表現を変更するための前段のリファクタリング

--- Assets/Scripts/Yeelight/Yeelight.cs
+++ Assets/Scripts/Yeelight/Yeelight.cs
@@ -9,9 +9,6 @@ public class Yeelight
     
     public static async void SwitchLight(bool isOn)
     {
-        var arg = isOn ? Random.Range(1, 8) : 0;
-        Debug.Log($"isOn = {isOn}, arg = {arg}");
-
         if (device == null)
         {
             device = new Device(YEELIGHT_IP);
@@ -19,27 +16,35 @@ public class Yeelight
             Debug.Log("Connected to Yeelight");
         }
         
-        if (arg == 0)
+        if (!isOn)
         {
-            await device.SetPower(isOn);
+            await device.SetPower(false);
         }
         else
         {
-            await device.SetPower(isOn);
-            await device.SetBrightness(10);
-            var color = arg switch
-            {
-                1 => new Color(255, 255,   0),
-                2 => new Color(255,   0, 255),
-                3 => new Color(  0, 255, 255),
-                4 => new Color(255,   0,   0),
-                5 => new Color(  0, 255,   0),
-                6 => new Color(  0,   0, 255),
-                _ => new Color(255, 255, 255)
-            };
-            await device.SetRGBColor((int)color.r, (int)color.g, (int)color.b);
+            TurnOnWithRandomColor();
         } 
 
         Debug.Log("Switched the light without python");
+    }
+
+    private static async void TurnOnWithRandomColor()
+    {
+        var arg = Random.Range(1, 8);
+        Debug.Log($"arg = {arg}");
+        
+        await device.SetPower();
+        // await device.SetBrightness(10);
+        var color = arg switch
+        {
+            1 => new Color(255, 255,   0),
+            2 => new Color(255,   0, 255),
+            3 => new Color(  0, 255, 255),
+            4 => new Color(255,   0,   0),
+            5 => new Color(  0, 255,   0),
+            6 => new Color(  0,   0, 255),
+            _ => new Color(255, 255, 255)
+        };
+        await device.SetRGBColor((int)color.r, (int)color.g, (int)color.b);
     }
 }
